ac95e4906199d2f157c22020e056ad68ed0f83d2
neels99 <vasile.danil1@gmail.com>
2021-02-15 00:03:56 +0300

    new tests

---
+++ build/par_deriv/CMakeFiles/shared_this_parent_derived.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shared_this_parent_derived.dir/shared_this_parent_derived.cpp.o"
+  "shared_this_parent_derived"
+  "shared_this_parent_derived.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shared_this_parent_derived.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
