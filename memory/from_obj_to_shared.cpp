17897d9ffa23b3fb047cfc6eeb5f6f368622b8cf
neels99 <vasile.danil1@gmail.com>
2021-02-26 00:36:46 +0300

    upd

---
+++ memory/from_obj_to_shared.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class A
+{
+public:
+    int i;
+    A(const A&) = delete;
+    A(int _i) : i(_i) {}
+};
+
+int main()
+{
+    A a(13);
+    
+    shared_ptr<A> _a = make_shared<A>(std::move(a));
+    // shared_ptr<A> _a2 = a;
+    cout << _a->i;
+}
