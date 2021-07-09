4a150d9306215a7c9d4ead1305f48e9387b2660a
neels99 <vasile.danil1@gmail.com>
2021-07-09 22:10:29 +0300

    upd

--- 2/pack1.cpp
+++ 2/pack1.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 using namespace std;
 
-#pragma pack(1)
+#pragma pack(push, 1)
 struct A
 {
     int a;
@@ -11,6 +11,15 @@ struct A
     int c;
     char str[9]; // = "hello";
 };
+#pragma pack(pop)
+
+struct B
+{
+    int b;
+    int c;
+    char str[9]; // = "hello";
+};
+
 
 //[int(x) for x in struct.pack('<iii9s', 300, 600, 900, b'hello!!1')]
 //[44, 1, 0, 0, 88, 2, 0, 0, 132, 3, 0, 0, 104, 101, 108, 108, 111, 33, 33, 49, 0]
@@ -39,4 +48,39 @@ int main()
     cout << endl;
 
     cout << "len: " << (sizeof(a) / sizeof(*a_bytes)) << endl;
+
+    cout << "==============================" << endl;
+
+
+    char s2[] = "hello!!1";
+    B b = {600, 900};
+    strcpy(b.str, s2);
+    // a.str = new char[strlen("hello")];
+
+    // strcpy(a.str, s);//, strlen(s));
+    cout << b.str << endl;
+    for (int i = 0; i < strlen(b.str); i++){
+        cout << (unsigned int)(((unsigned char*)b.str)[i]) << " ";
+    }
+    cout << endl;
+    unsigned char *b_bytes_temp;
+    unsigned char *b_bytes;
+    b_bytes_temp = reinterpret_cast<unsigned char *>(&b);
+    b_bytes = b_bytes_temp;
+
+    int len2 = (sizeof(b) / sizeof(*b_bytes));
+    for (int i = 0; i < len2; i++)
+    {
+        cout << (unsigned int)b_bytes[i] << " ";
+    }
+    cout << endl;
+
+    cout << "len: " << (sizeof(b) / sizeof(*b_bytes)) << endl;
+
+    //===========================================================
+
+    B *b_unpack;
+    b_unpack = (B*)b_bytes;
+
+    cout << "unpack res: " << b_unpack->str << " " << b_unpack->b << " " << b_unpack->c << endl;
 }
